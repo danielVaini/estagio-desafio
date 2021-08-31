79ca5fd9b432295c0d0004d13df14dddd9cf1aaa
Daniel Vaini <dnlvaini@gmail.com>
2021-08-30 23:05:31 -0300

    Creating the event model

---
+++ backend/src/models/Event.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Event = new Schema({
+  desc: {
+    type: String,
+    required: true,
+  },
+  start_time: {
+    type: String,
+    required: true,
+  },
+  end_time: {
+    type: String,
+    required: true
+  }
+})
+
+mongoose.model('events', Event)
