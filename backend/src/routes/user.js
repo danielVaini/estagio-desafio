26999627fc326bddabf7439dd394e2405a5617df
Daniel Vaini <dnlvaini@gmail.com>
2021-09-02 00:15:11 -0300

    Criando rotas para usuários

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,44 +1,87 @@
-const mongoose = require('mongoose');
-const express = require('express');
-require('../models/User');
-const User = mongoose.model('users')
-
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const bcryptjs = require("bcryptjs");
+const passport = require('passport')
+require("../models/User");
+const User = mongoose.model("users");
 
-router.get('/users', (req, res) => {
+router.get("/cadastro", (req, res) => {
   User.find().then((result) => {
-    res.send(result)
-  })
-})
+    res.send(result);
+  });
+});
 
-router.post('/users/add', (req, res) => {
-  const newEvent = { 
+router.post("/cadastro", (req, res) => {
+  const newUser = new User({
     name: req.body.name,
     password: req.body.password,
     email: req.body.email,
-  }
+  });
 
-  new User(newEvent).save().then((result) => {
-    console.log('User registred')
-    return res.send(result).status(201)
-  }).catch((error) => {
-    console.log("Error")
-  })
-})
+  User.findOne({ email: req.body.email }).then((result) => {
+    if (result){
+      
+      return res.send({ message: "Erro, usuário já existe com este e-mail " });
+    }
 
-router.post('/users/edit', (req, res) => {
-  User.findOne({_id: req.body.id}).then((user) => {
-    user.name = req.body.name;
+    bcryptjs.genSalt(10, (error, salt) => {
+      bcryptjs.hash(newUser.password, salt, (error, hash) => {
+        if (error) {
+          res.redirect("/");
+        }
 
-    user.save().then((result) => {
-      console.log('Edit')
-      res.send(result)
-    }).catch((error) => {
-      res.send(error)
+        newUser.password = hash;
+
+        User(newUser)
+          .save()
+          .then((result) => {
+            console.log("User registred");
+            res.send(result).status(201);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    });
+  }).catch(error => res.status(400).send({messagem: "Erro interno"}))
+});
+
+router.post("/cadastro", (req, res) => {
+  User.findOne({ _id: req.body.id })
+    .then((user) => {
+      user.name = req.body.name;
+
+      user
+        .save()
+        .then((result) => {
+          console.log("Edit");
+          res.send(result);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
     })
-  }).catch((error) => {
-    res.send(error)
-  })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/login',
+    failureRedirect: '/error',
+  
+  })(req, res, next)
 })
 
-module.exports = router
+router.get('/login', (req, res) => {
+  res.send({msg: "Logado"})
+})
+router.get('/error', (req, res) => {
+  res.send({msg: "Error"})
+})
+
+
+
+module.exports = router;
