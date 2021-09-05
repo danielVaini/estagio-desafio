100fb23d0a16f07422bbc5f09a792e16d158299e
Daniel Vaini <dnlvaini@gmail.com>
2021-09-05 11:35:45 -0300

    component de Eventos

---
+++ frontend/src/pages/Eventos/Eventos.tsx
@@ -0,0 +1,18 @@
+
+import Nav from '../../components/Nav';
+
+import ListEvents from './ListEvents';
+
+
+
+const Eventos = () => {
+  return (
+    <div className="Eventos container-fluid">
+      <Nav />
+ 
+      <ListEvents />
+    </div>
+  )
+}
+
+export default Eventos;
