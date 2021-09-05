083c0fdd99357e5ace5848ec10ef69eff8cf78bb
Daniel Vaini <dnlvaini@gmail.com>
2021-09-05 11:37:40 -0300

    Adicionando componente de rotas no arquivo principal

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './Global.css'
+import Routes from './pages/routes/routes';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+       <Routes />
+   
+    </div>
+  );
+}
+
+export default App;
