806eefd8ab6dd5c9e3a05e55f212f3e8b9b4f706
Daniel Vaini <dnlvaini@gmail.com>
2021-09-05 13:06:15 -0300

    Editando menu de navegação

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
@@ -6,18 +7,16 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">EventsApp</a>
+        <Link to="/events" className="navbar-brand" href="#">EventsApp</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Login</a>
+              <Link className="nav-link active" aria-current="page" to="/sobre">Sobre</Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Cadastro</a>
-            </li>
+         
 
 
           </ul>
