e85c83902f91e81cabd7ac0d0b535535705f0b72
Daniel Vaini <dnlvaini@gmail.com>
2021-09-05 13:07:55 -0300

    Formatando código

--- frontend/src/pages/Eventos/FormEventos.tsx
+++ frontend/src/pages/Eventos/FormEventos.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import api from '../../server/api';
 
 import AddIcon from '../../assets/plus-square.svg'
@@ -9,44 +9,74 @@ const FormEventos = () => {
   const [end_time, setEndTime] = useState('')
   const [date, setDate] = useState('')
 
-  function handleAddevent() {
+  function handleAddevent(e: FormEvent) {
+    e.preventDefault()
     const novaData = new Date(date)
-    const dataFormatada = novaData.toLocaleDateString('pt-BR', {timeZone: 'UTC'})
+    const dataFormatada = novaData.toLocaleDateString('pt-BR', { timeZone: 'UTC' })
 
-
-    api.post('/events/add', 
-            { desc, 
-              start_time, 
-              end_time, 
-              date: dataFormatada 
-            })
-            .then()
-            .catch(error => window.alert('Erro'))
+    api.post('/events/add',
+      {
+        desc,
+        start_time,
+        end_time,
+        date: dataFormatada
+      })
+      .then(() => {
+        window.alert("Cadastrasdo com sucesso")
+      })
+      .catch(error => window.alert('Erro ao cadastrar'))
 
   }
 
   return (
-    <div role="form" className="FormEventos mt-4">
+    <form className="FormEventos mt-4">
       <div className="container-fluid">
         <div className="row align-items-end">
           <div className="col-5">
             <label htmlFor="title">Titulo</label>
-            <input type="text" className="form-control cm-input" id="title" name="desc" onChange={e => setDesc(e.target.value)} />
+            <input
+              type="text"
+              className="form-control cm-input"
+              id="title"
+              name="desc"
+              onChange={e => setDesc(e.target.value)}
+              required
+            />
           </div>
           <div className="col-2">
             <label htmlFor="start-time">Inicio</label>
-            <input type="time" className="form-control cm-input" id="start-time" name="start_time" onChange={e => setStartTime(e.target.value)} />
+            <input
+              type="time"
+              className="form-control cm-input"
+              id="start-time" name="start_time"
+              onChange={e => setStartTime(e.target.value)}
+              required
+            />
           </div>
           <div className="col-2">
             <label htmlFor="end-time">Fim</label>
-            <input type="time" className="form-control cm-input" id="end-time" name="end_time" onChange={e => setEndTime(e.target.value)} />
+            <input
+              type="time"
+              className="form-control cm-input"
+              id="end-time"
+              name="end_time"
+              onChange={e => setEndTime(e.target.value)}
+              required
+            />
           </div>
           <div className="col-2">
             <label htmlFor="date">Data</label>
-            <input type="date" className="form-control cm-input"  id="date" name="date" onChange={e => setDate(e.target.value)} />
+            <input
+              type="date"
+              className="form-control cm-input"
+              id="date"
+              name="date"
+              onChange={e => setDate(e.target.value)}
+              required
+            />
           </div>
           <div className="col-1">
-            <button className="btn btn-success align-middle" onClick={() => handleAddevent()}>
+            <button className="btn btn-success align-middle" type="submit" onClick={(e) => handleAddevent(e)}>
               <img src={AddIcon} alt="Icone adicionar" />
             </button>
           </div>
@@ -54,7 +84,7 @@ const FormEventos = () => {
 
       </div>
 
-    </div>
+    </form>
   )
 }
 
