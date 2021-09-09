c95af0b014fb3ea6d6bfe0f3d6e019bab5793829
Daniel Vaini <dnlvaini@gmail.com>
2021-09-09 00:05:42 -0300

    Adicionando componente de rotas privadas

--- frontend/src/pages/routes/routes.tsx
+++ frontend/src/pages/routes/routes.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Cadastro from '../Cadastro/Cadastro';
 import Edit from '../Edit/Edit';
 import Eventos from '../Eventos/Eventos';
 import Login from '../Login/Login';
 import Sobre from '../Sobre/Sobre';
+import PrivateRouter from './privateRouter';
 
+import AuthContext from '../../data/store/auth';
 
 
 const Routes = () => {
+
+  const context = useContext(AuthContext)
   return (
     <BrowserRouter>
       <Switch>
@@ -16,9 +20,12 @@ const Routes = () => {
       
       <Route path="/login" component={Login} />
       <Route path="/cadastro"  component={Cadastro} />
-      <Route path="/events" exact component={Eventos} />
-      <Route path="/events/edit/:id"  component={Edit} />
+      {/* <Route path="/events" exact component={Eventos} /> 
+      <Route path="/events/edit/:id"  component={Edit} /> */}
       <Route path="/sobre"  component={Sobre} />
+
+      <PrivateRouter path="/events" component={Eventos} />
+      <PrivateRouter path="/events/edit/:id"  component={Edit} />
       </Switch>
     </BrowserRouter>
     )
